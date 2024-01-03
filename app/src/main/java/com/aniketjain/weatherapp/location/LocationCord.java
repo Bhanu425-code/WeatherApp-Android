97751511099a5708878d7ad417060d071e0acf8b
Bhanu Shree Rekha Gullapalli <bhanu2003gullapalli@gmail.com>
2024-01-03 13:39:17 +0530

    Introducing Weatherly, your go-to Android weather app for accurate and up-to-date weather information. With a sleek and intuitive design, Weatherly provides a seamless user experience, keeping you informed about the latest weather conditions wherever you are.

---
+++ app/src/main/java/com/aniketjain/weatherapp/location/LocationCord.java
@@ -0,0 +1,9 @@
+package com.aniketjain.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+        public final static String API_KEY = "3be28164f73fa1b8dbdbd290496f92b9";
+//    public final static String API_KEY = "eeb8b40367eee691683e5a079e2fa695";
+}
+
